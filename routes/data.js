03868e63202a425c852171ba0665b3eb34705fc7
Christoph Neuroth <christoph.neuroth@gmail.com>
2012-09-15 17:52:23 +0200

    consistency: 2 spaces for indentation

--- routes/data.js
+++ routes/data.js
@@ -1,35 +1,35 @@
 var uuid = require('node-uuid');
 module.exports = function(options) {
-    var storage = options.storage
-      , publish = options.publish
-      ;
-    return {
-        create: function (request, response) {
-            var id = uuid.v1();
-            storage.create(id);
-            response.status(201).send({
-                id: id
-            });
-        },
+  var storage = options.storage
+    , publish = options.publish
+    ;
+  return {
+    create: function (request, response) {
+      var id = uuid.v1();
+      storage.create(id);
+      response.status(201).send({
+        id: id
+      });
+    },
 
-        post_id: function (request, response) {
-            var data = request.body
-              , id = request.params.id;
-            storage.add(id, data);
-            publish(id);
-            response.status(200).send();
-        },
+    post_id: function (request, response) {
+      var data = request.body
+        , id = request.params.id;
+      storage.add(id, data);
+      publish(id);
+      response.status(200).send();
+    },
 
-        put_id: function (request, response) {
-            var data = request.body
-              , id = request.params.id;
-            storage.set(request.params.id, data);
-            publish(id);
-            response.status(200).send();
-        },
+    put_id: function (request, response) {
+      var data = request.body
+        , id = request.params.id;
+      storage.set(request.params.id, data);
+      publish(id);
+      response.status(200).send();
+    },
 
-        get_by_id: function(request, response) {
-            response.status(200).send(storage.get(request.params.id));
-        }
-    };
+    get_by_id: function(request, response) {
+      response.status(200).send(storage.get(request.params.id));
+    }
+  };
 };
